72854073d3afd75301008695634ab72928f971fd
Nenkai <Nenkai@users.noreply.github.com>
2025-05-05 01:16:22 +0200

    bump version (& display automatically)

--- RelinkToolkit2/Views/MainWindow.axaml.cs
+++ RelinkToolkit2/Views/MainWindow.axaml.cs
@@ -4,6 +4,8 @@ using CommunityToolkit.Mvvm.Messaging;
 
 using RelinkToolkit2.Messages.Dialogs;
 
+using System.Reflection;
+
 namespace RelinkToolkit2.Views;
 
 public partial class MainWindow : Window
@@ -11,6 +13,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        string versionString = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString(3) ?? "unknown version";
+        this.Title += $" (v{versionString})";
 
         WeakReferenceMessenger.Default.Register<ShowDialogRequest>(this, (recipient, message) =>
         {
